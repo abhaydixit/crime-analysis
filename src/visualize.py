6ead4b0aeb16140b517200b6cde9b8e478b90cbb
Abhay Rajendra Dixit <abhaydixit95@gmail.com>
2020-11-18 12:30:17 -0500

    Modified Visualize.py

--- src/visualize.py
+++ src/visualize.py
@@ -1,16 +1,12 @@
 import sys
-import time
 import matplotlib.pyplot as plt
 from src import connection as conn
 import pandas as pd
 
 
-
 MONTH_NUMBER = {1: "Jan", 2: "Feb", 3: "Mar", 4: "Apr", 5: "May", 6: "Jun", 7: "Jul", 8: "Aug", 9: "Sep",
                        10: "Oct", 11: "Nov", 12: "Dec"}
 YEAR = {2011: '11', 2012: '12', 2013: '13', 2014: '14', 2015: '15', 2016: '16', 2017: '17', 2018: '18', 2019: '19', 2020: '20'}
-
-
 
 
 def pie_chart(mongo_coln):
@@ -38,12 +34,10 @@ def pie_chart(mongo_coln):
         }
     ]))
 
-
-    print(crime_series)
     austin_dict = {'city': 'Austin', "offense": [], "crime count": []}
     baltimore_dict = {'city': 'Baltimore', "offense": [], "crime count": []}
     chicago_dict = {'city': 'Chicago', "offense": [], "crime count": []}
-    lacity_dict = {'city': 'LA City', "offense": [], "crime count": []}
+    lacity_dict = {'city': 'LACity', "offense": [], "crime count": []}
     roch_dict = {'city': 'Rochester', "offense": [], "crime count": []}
 
     for item in crime_series:
@@ -56,7 +50,7 @@ def pie_chart(mongo_coln):
         elif item['_id']['city'] == "Chicago":
             chicago_dict['offense'].append(item['_id']['offense'])
             chicago_dict['crime count'].append(item['count'])
-        elif item['_id']['city'] == "LA City":
+        elif item['_id']['city'] == "LACity":
             lacity_dict['offense'].append(item['_id']['offense'])
             lacity_dict['crime count'].append(item['count'])
         elif item['_id']['city'] == "Rochester":
@@ -68,11 +62,11 @@ def pie_chart(mongo_coln):
         labels = city['offense']
         sizes = city['crime count']
         plt.figure()
-        # explode = (0, 0.1, 0, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
-        plt.pie(sizes, labels=labels, autopct='%1.1f%%', startangle=90)
+        plt.title(city['city'])
+        plt.pie(sizes, labels=labels, startangle=90)
+        plt.legend(loc='best', labels=['%s, %1.1f %%' % (l, (s/sum(sizes))*100) for l, s in zip(labels, sizes)])
         plt.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
-
-    plt.show()
+    # plt.show()
 
 
 def month_series(mongo_coln):
@@ -101,13 +95,10 @@ def month_series(mongo_coln):
         {'$sort': {"_id.month": 1}},
     ]))
 
-    print(crime_series)
-    # crime_series = list(filter(lambda x: 2019 >= x['_id']['year'] > 2012, crime_series))
-
     austin_dict = {'city': 'Austin', "month": [], "crime count": []}
     baltimore_dict = {'city': 'Baltimore', "month": [], "crime count": []}
     chicago_dict = {'city': 'Chicago', "month": [], "crime count": []}
-    lacity_dict = {'city': 'LA City', "month": [], "crime count": []}
+    lacity_dict = {'city': 'LACity', "month": [], "crime count": []}
     roch_dict = {'city': 'Rochester', "month": [], "crime count": []}
 
     for item in crime_series:
@@ -120,7 +111,7 @@ def month_series(mongo_coln):
         elif item['_id']['city'] == "Chicago":
             chicago_dict['month'].append(MONTH_NUMBER[item['_id']['month']])
             chicago_dict['crime count'].append(item['count'])
-        elif item['_id']['city'] == "LA City":
+        elif item['_id']['city'] == "LACity":
             lacity_dict['month'].append(MONTH_NUMBER[item['_id']['month']])
             lacity_dict['crime count'].append(item['count'])
         elif item['_id']['city'] == "Rochester":
@@ -128,7 +119,7 @@ def month_series(mongo_coln):
             roch_dict['crime count'].append(item['count'])
 
 
-    f1 = plt.figure(1)
+    plt.figure()
     plt.plot(austin_dict['month'], austin_dict['crime count'], label=austin_dict['city'])
     plt.plot(baltimore_dict['month'], baltimore_dict['crime count'], label=baltimore_dict['city'])
     plt.plot(chicago_dict['month'], chicago_dict['crime count'], label=chicago_dict['city'])
@@ -137,10 +128,10 @@ def month_series(mongo_coln):
 
     plt.xlabel('Month')
     plt.ylabel('Number of crimes')
-    plt.title('Time series of number of crimes committed in different cities')
+    plt.title('Number of crimes committed in different cities in different months')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    # plt.show()
 
 
 def month_year_time_series(mongo_coln):
@@ -174,7 +165,7 @@ def month_year_time_series(mongo_coln):
     austin_dict = {'city': 'Austin', "date": [], "crime count": []}
     baltimore_dict = {'city': 'Baltimore', "date": [], "crime count": []}
     chicago_dict = {'city': 'Chicago', "date": [], "crime count": []}
-    lacity_dict = {'city': 'LA City', "date": [], "crime count": []}
+    lacity_dict = {'city': 'LACity', "date": [], "crime count": []}
     roch_dict = {'city': 'Rochester', "date": [], "crime count": []}
 
     for item in crime_series:
@@ -187,7 +178,7 @@ def month_year_time_series(mongo_coln):
         elif item['_id']['city'] == "Chicago":
             chicago_dict['date'].append(MONTH_NUMBER[item['_id']['month']] + " " + YEAR[item['_id']['year']])
             chicago_dict['crime count'].append(item['count'])
-        elif item['_id']['city'] == "LA City":
+        elif item['_id']['city'] == "LACity":
             lacity_dict['date'].append(MONTH_NUMBER[item['_id']['month']] + " " + YEAR[item['_id']['year']])
             lacity_dict['crime count'].append(item['count'])
         elif item['_id']['city'] == "Rochester":
@@ -195,7 +186,7 @@ def month_year_time_series(mongo_coln):
             roch_dict['crime count'].append(item['count'])
 
 
-    f1 = plt.figure(1)
+    plt.figure()
     plt.plot(austin_dict['date'], austin_dict['crime count'], label=austin_dict['city'])
     plt.plot(baltimore_dict['date'], baltimore_dict['crime count'], label=baltimore_dict['city'])
     plt.plot(chicago_dict['date'], chicago_dict['crime count'], label=chicago_dict['city'])
@@ -211,13 +202,13 @@ def month_year_time_series(mongo_coln):
 
 
 
-    f2 = plt.figure(2)
+    plt.figure()
 
     austin_df = pd.DataFrame(austin_dict)
     austin_df['sc_austin'] = austin_df.iloc[:, 2].rolling(window=4).mean()
 
     baltimore_df = pd.DataFrame(baltimore_dict)
-    baltimore_df['sc_baltimore'] = austin_df.iloc[:, 2].rolling(window=4).mean()
+    baltimore_df['sc_baltimore'] = baltimore_df.iloc[:, 2].rolling(window=4).mean()
 
     chicago_df = pd.DataFrame(chicago_dict)
     chicago_df['sc_chicago'] = chicago_df.iloc[:, 2].rolling(window=9).mean()
@@ -231,7 +222,7 @@ def month_year_time_series(mongo_coln):
     plt.plot(austin_df['date'], austin_df['sc_austin'], label="Austin")
     plt.plot(baltimore_df['date'], baltimore_df['sc_baltimore'], label="Baltimore")
     plt.plot(chicago_df['date'], chicago_df['sc_chicago'], label="Chicago")
-    plt.plot(lacity_df['date'], lacity_df['sc_lacity'], label="LA City")
+    plt.plot(lacity_df['date'], lacity_df['sc_lacity'], label="LACity")
     plt.plot(roch_df['date'], roch_df['sc_roch'], label="Rochester")
 
     plt.xlabel('Month-Year')
@@ -240,7 +231,7 @@ def month_year_time_series(mongo_coln):
     plt.title('Time series of number of crimes committed in different cities')
     plt.legend()
     plt.grid(True)
-    plt.show()
+    # plt.show()
 
 
 def main():
@@ -251,22 +242,18 @@ def main():
             the overall time taken.
         """
         config_file_path = '../config'
-        # dir_path = get_dataset_path(config_file_path + '/path.json')
 
         mongo_connection_params = conn.get_mongo_parameters(config_file_path + '/connection.json')
         print("Using Mongo Connection Params as: ", mongo_connection_params)
         mongo_database = conn.get_mongo_connection(mongo_connection_params)
-        mongo_coln = mongo_database['Crime Analysis by City']
-
-        # Expand the cursor and construct the DataFrame
-        # df = pd.DataFrame(list(cursor))
-        # print(df.head(10))
+        mongo_coln = mongo_database['city_crimes']
 
 
         # Time series
-        # month_year_time_series(mongo_coln)
-        # month_series(mongo_coln)
+        month_year_time_series(mongo_coln)
+        month_series(mongo_coln)
         pie_chart(mongo_coln)
+        plt.show()
 
 
 if __name__ == "__main__":
